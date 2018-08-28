7a5a980d5dc1d9f94e7ee304e580ae73661523dd
KYIV\vpak <pahomovvladimir1@gmail.com>
2018-08-28 19:52:31 +0300

    Feat : Data added Doc :webpack.config.js restored to the stage before data, all working properly.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     let element = document.createElement('div');
@@ -12,8 +13,10 @@ function component() {
    myIcon.src = Icon;
 
    element.appendChild(myIcon);
+   
+   console.log(Data);
 
-     return element;
+   return element;
   }
   
   document.body.appendChild(component());
